1a37d45fe5fa7cdb74ad2b035e7df3ae5ad27e41
Vardi Narendra <narendravcool@gmail.com>
2015-09-14 11:10:57 +0530

    Update wrong_line_index.py

--- wrong_line_index.py
+++ wrong_line_index.py
@@ -1,13 +1,9 @@
 #developed by Narendra Vardi
-
 #Motivation for developing this file is that I have solved problems in HackerRank for which I got partial output.
-#When I download the HackerRank testcase and try
-#to check where I went wrong, The Output file will be so complex that it becomes very tough for me to 
+#When I download the HackerRank testcase and try to check where I went wrong, The Output file will be so complex that it becomes very tough for me to 
 # find the line where i am going wrong, so I wrote this program which gives me the line number where I went wrong.
-
-#**save your HackerRank output in hrop.out
-
-#**save your output in op.out
+#save your HackerRank output in hrop.out
+#save your output in op.out
 #Just run this file, This will give the line number where there is change.
 # this will print all the line count for all the lines that mismatch
 #**This program has been developed assuming that both files will have same number of lines
@@ -28,3 +24,9 @@ for i in xrange(0,len(outputlist)):
 		count+=1
 	else:
 		count+=1
+
+hroutput.close()
+output.close()	
+
+
+
